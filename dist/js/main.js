06f4f41eb2a7569c9849cde67a05129b0e0e16cd
Mon Plaza <monmon.plaza@gmail.com>
2018-09-06 15:10:53 +0800

    done

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         nav: true,
         pause: false,
         pager: true,
-        auto: false
+        auto: true
     });
 
     $('.next').html('&#0155');
@@ -12,6 +12,64 @@ $(document).ready(function () {
 
 
     $('.toggle-menu').click(function () {
+        $(this).toggleClass('close');
         $('nav').toggleClass('active');
     });
+
+    $('nav ul li').click(function () {
+        $('nav').removeClass('active');
+    });
+
+
+
+
+
+    $(window).scroll(function () {
+        var scrollPos = $(window).scrollTop();
+        $('.banner-promo').css('top', 50 + (scrollPos) * .09 + '%');
+        $('.banner-main').css('top', 50 + (scrollPos) * .09 + '%');
+        $('#about').css('background-position', 'center ' + (scrollPos) * .015 + 'px');
+    });
+
+
+    $('.open-gallery').click(function () {
+        var items = [];
+        $($(this).attr('href')).find('.slide').each(function () {
+            items.push({
+                src: $(this)
+            });
+        });
+        $.magnificPopup.open({
+            items: items,
+            gallery: {
+                enabled: true
+            }
+        });
+    });
+
+    var scrollLink = $('.nav-item');
+
+    scrollLink.click(function (e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 500);
+    })
+
+    $(window).scroll(function () {
+        var scrollPos = $(this).scrollTop();
+        scrollLink.each(function () {
+            var sectionOffset = $(this.hash).offset().top - 30;
+            if (sectionOffset <= scrollPos) {
+                $(this).parent().addClass('active')
+                $(this).parent().siblings().removeClass('active')
+            }
+        })
+
+
+    })
+
+
+
+
 });
